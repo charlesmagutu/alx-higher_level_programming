818d8a70a69447277c45b3acda9df682b800d7f3
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-12 13:58:11 -0700

    fixed 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ if number >= 0:
 
 last = number % 10 if number >= 0 else (abs(number) % 10) * -1
 if last > 5:
-    print("Last value of {} is {} and is greater than 5".format(number, last))
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
 elif last == 0:
-    print("Last value of {} is {} and is 0".format(number, last))
+    print("Last digit of {} is {} and is 0".format(number, last))
 elif last < 6 and not 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
