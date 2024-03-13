7c186108a9179104ac77a65639aeadf19b65ea02
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-12 23:21:23 -0700

    fixed 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-print("01", end="")
-for i in range(2, 10):
+for i in range(0, 8):
     for j in range(i + 1, 10):
-        print(", {:02d}".format(int(str(i) + str(j))), end="")
-print()
+        print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}{:d}".format(i + 1, j))
