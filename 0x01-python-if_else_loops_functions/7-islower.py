063283279f0249bed4d7180e4ffa7c42dee5e995
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-12 23:26:32 -0700

    fixed 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in c:
-        if ord('a') <= ord(char) <= ord('z') or char.isdigit():
-            return True
-    return False
+    return (ord('a') <= ord('c') <=ord('z'))
