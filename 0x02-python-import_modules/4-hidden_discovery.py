5bf419f88c93bcc91589fb502f51ec311318bcaa
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-14 10:38:24 -0700

    formted code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def check():
     for a in dir(hidden_4):
         if a[:2] != '__':
             print("{:s}".format(a))
+
+
 if __name__ == '__main__':
     check()
-
