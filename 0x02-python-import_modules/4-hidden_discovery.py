2d460baeda8217c41116562801f4ee4bc1b9f749
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-14 10:35:57 -0700

    updated script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
 
-for a in dir(hidden_4):
-    if not a.startswith('__'):
-        print(a)
+def check():
+    for a in dir(hidden_4):
+        if a[:2] != '__':
+            print("{:s}".format(a))
+if __name__ == '__main__':
+    check()
+
