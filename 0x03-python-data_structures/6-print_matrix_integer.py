ae9d08ada8c3c7f044e947f3407b13fa37e9f4ab
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-18 05:45:28 -0700

    used str.format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@
 def print_matrix_integer(matrix=[[]]):
     transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
     for row in transposed:
-        print(" ".join(map(str, row)))
+        print("{}".format(" ".join(map(str, row)), end=" "))
