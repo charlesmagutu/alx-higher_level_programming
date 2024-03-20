e2f7e3ef0336069acefb2f5ec76a8e7800fd408a
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-20 13:05:46 -0700

    fixed file 01

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,8 @@
 
 
 def search_replace(my_list, search, replace):
+    new_lis = my_list[:]
     for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+        if new_lis[i] == search:
+            new_lis[i] = replace
+    return new_lis
