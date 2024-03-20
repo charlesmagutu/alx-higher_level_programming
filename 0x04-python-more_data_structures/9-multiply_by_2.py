1c1f6c8bd5df7e74a6dc10158a608a1ea44f7f5d
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-20 13:14:22 -0700

    fixed 09

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
     for key in new_dic.keys():
-        new_dic[key] *=2
-        return new_dic
+        new_dic[key] *= 2
+    return new_dic
