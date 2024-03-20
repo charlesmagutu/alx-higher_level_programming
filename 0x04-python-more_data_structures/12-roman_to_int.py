49ded751174f0563b3b2ac51a9311b6ee595e07c
charlesmagutu <charlesmagutu11@gmail.com>
2024-03-20 12:55:36 -0700

    fixed indent on file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,6 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
-
     roman_dict = {
             "I": 1,
             "V": 5,
@@ -16,7 +15,7 @@ def roman_to_int(roman_string):
             "C": 100,
             "D": 500,
             "M": 1000
-    }
+            }
     num = 0
 
     for i in range(len(roman_string)):
@@ -25,8 +24,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+            num += roman_dict[roman_string[i]] * -1
         else:
             num += roman_dict[roman_string[i]]
     return (num)
