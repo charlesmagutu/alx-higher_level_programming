0fa7c86d78645dd210d07ba397d7bdf9732ea7b1
charlesmagutu <charlesmagutu11@gmail.com>
2024-04-03 05:51:55 -0700

    added documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Represents a square shape.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__: Initializes a new instance of the Square class.
+    """
 
     def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+        """
         pass
+
